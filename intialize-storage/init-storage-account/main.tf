8aa0703a1ff3374cdd2faaaee1a6dac03e521609
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-01-27 20:30:22 +0800

    Save main.tf.

--- intialize-storage/init-storage-account/main.tf
+++ intialize-storage/init-storage-account/main.tf
@@ -19,7 +19,7 @@ resource "random_string" "resource_code" {
 
 resource "azurerm_resource_group" "tfstate" {
   name     = "gni-tfstate"
-  location = "East US"
+  location = var.location
 }
 
 resource "azurerm_storage_account" "tfstate" {
