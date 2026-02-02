2a1110c6812700948452b8de8a386fa6b140e814
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-02 21:05:38 +0800

    Save main.tf.

---
+++ initializer/main.tf
@@ -0,0 +1,38 @@
+locals {
+  loc_name = replace(lower(var.location), " ", "")
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "tfstate_rg" {
+  name     = "${var.org}-${var.infra_env}-${local.loc_name}-tfstate-rg"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "tfstate_sa" {
+  name = lower(
+    "${var.org}${var.infra_env}${local.loc_name}tfstate${random_string.resource_code.result}"
+  )
+
+  resource_group_name      = azurerm_resource_group.tfstate_rg.name
+  location                 = azurerm_resource_group.tfstate_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = var.infra_env
+    name        = "${var.org}-${var.infra_env}-${local.loc_name}-tfstate"
+    group       = "gni-applications"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate_store" {
+  name                  = "${var.org}-${var.infra_env}-${local.loc_name}-tfstate-store"
+  storage_account_id    = azurerm_storage_account.tfstate_sa.id
+  container_access_type = "private"
+}
