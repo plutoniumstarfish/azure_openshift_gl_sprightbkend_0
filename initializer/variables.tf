879fd1c132512764c94184936b0e47671e1cf5a6
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-02 20:27:42 +0800

    Save variables.tf.

--- initializer/variables.tf
+++ initializer/variables.tf
@@ -1,5 +1,17 @@
 variable "location" {
     type = string
-    description = "Region to deploy resources"
+    description = "Location to deploy resources"
     default = "EAST US"
+}
+
+variable "infra_env" {
+    type = string
+    description = "subscription environment dev, npr, prp, prd"
+    default = "dev"
+    
+    
+variable "organization" {
+    type = string
+    description = "organization/company"
+    default = "gni"
 }
