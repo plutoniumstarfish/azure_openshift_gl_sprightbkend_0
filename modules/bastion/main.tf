f5bcc73c3ec1bb51916d7c1339b5ab03552b7e49
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-06 19:33:40 +0800

    Save main.tf.

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,10 +1,7 @@
 # Create Resource Group
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
+resource "azurerm_resource_group" "bastion_rg" {
+  location = var.location
   name     = random_pet.rg_name.id
 }
 
