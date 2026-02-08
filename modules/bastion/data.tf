151099d9b9256bf518c628d78c8d50ec3bcb1d41
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-09 05:17:25 +0800

    Save data.tf.

--- modules/bastion/data.tf
+++ modules/bastion/data.tf
@@ -2,15 +2,14 @@ locals {
   loc_name = replace(lower(var.location), " ", "")
 }
 
-
 data "azurerm_resource_group" "shared_rg" {
   name = "${var.org}-${var.infra_env}-${local.loc_name}-shared-rg"
 }
 
-
-# 2. Look up the Virtual Network created in Module 1
+# 2. Look up the Virtual Network (added resource_group_name)
 data "azurerm_virtual_network" "shared_vnet" {
   name                = "${var.org}-${var.infra_env}-${local.loc_name}-shared-vnet"
+  resource_group_name = data.azurerm_resource_group.shared_rg.name
 }
 
 # 3. Look up the specific Bastion Subnet
