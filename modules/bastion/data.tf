656b9b7a668d17e3f59c9b3b52de88c4b5b95d76
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-09 05:06:40 +0800

    Save data.tf.

--- modules/bastion/data.tf
+++ modules/bastion/data.tf
@@ -2,9 +2,3 @@
 data "azurerm_resource_group" "shared_rg" {
   name = "shared-rg"
 }
-
-resource "azurerm_bastion_host" "example" {
-  name                = "my-bastion"
-  resource_group_name = data.azurerm_resource_group.shared_rg.name
-  location            = data.azurerm_resource_group.shared_rg.location
-  #
